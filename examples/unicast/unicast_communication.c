2f18bfe78157149abf8f869dc5f02382e7994b5b
Timothy Young <timothyoung97@gmail.com>
2024-03-28 16:14:57 +0800

    Receiver code

--- examples/unicast/unicast_communication.c
+++ examples/unicast/unicast_communication.c
@@ -20,19 +20,28 @@ static linkaddr_t dest_addr =         {{ 0x00, 0x12, 0x4b, 0x00, 0x11, 0xa7, 0x7
 PROCESS(unicast_process, "One to One Communication");
 AUTOSTART_PROCESSES(&unicast_process);
 
+int receiveCount = 0;
+int rssiTotal = 0;
+
 /*---------Callback executed immediately after reception---------*/
 void input_callback(const void *data, uint16_t len,
   const linkaddr_t *src, const linkaddr_t *dest) 
 {
+  receiveCount++;
   if(len == sizeof(unsigned)) {
     unsigned count;
     memcpy(&count, data, sizeof(count));
     printf("Received %u with rssi %d from", count, (signed short)packetbuf_attr(PACKETBUF_ATTR_RSSI));
-    linkaddr_t addr = *src;
-    printf("%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X\n", 
-           addr.u8[0], addr.u8[1], addr.u8[2], addr.u8[3], 
-           addr.u8[4], addr.u8[5], addr.u8[6], addr.u8[7]);
-    printf("\n");
+    printf(" Current count: %d\n", receiveCount);
+    
+    rssiTotal += (signed short)packetbuf_attr(PACKETBUF_ATTR_RSSI);
+    int avgRssi = rssiTotal / receiveCount;
+    printf("Average RSSI: %d\n", avgRssi);
+    // linkaddr_t addr = *src;
+    // printf("%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X\n", 
+    //        addr.u8[0], addr.u8[1], addr.u8[2], addr.u8[3], 
+    //        addr.u8[4], addr.u8[5], addr.u8[6], addr.u8[7]);
+    printf("                       ");
   }
 }
 /*---------------------------------------------------------------------------*/
@@ -40,7 +49,6 @@ PROCESS_THREAD(unicast_process, ev, data)
 {
   static struct etimer periodic_timer;
   static unsigned count = 0;
-
   PROCESS_BEGIN();
 
 
@@ -55,11 +63,11 @@ PROCESS_THREAD(unicast_process, ev, data)
     while(1) {
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
       printf("Sending %u to ", count);
-      linkaddr_t addr = dest_addr;
-      printf("%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X\n", 
-             addr.u8[0], addr.u8[1], addr.u8[2], addr.u8[3], 
-             addr.u8[4], addr.u8[5], addr.u8[6], addr.u8[7]);
-      printf("\n");
+      // linkaddr_t addr = dest_addr;
+      // printf("%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X\n", 
+      //        addr.u8[0], addr.u8[1], addr.u8[2], addr.u8[3], 
+      //        addr.u8[4], addr.u8[5], addr.u8[6], addr.u8[7]);
+      // printf("\n");
 
       NETSTACK_NETWORK.output(&dest_addr); //Packet transmission
       count++;
